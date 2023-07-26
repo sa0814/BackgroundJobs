de126d2c0b6f4cbafc293826327129d1740415df
sa0814 <92997651+sa0814@users.noreply.github.com>
2023-07-26 14:14:13 -0400

    Add files via upload

---
+++ test_dc_simulink_repo-main 4/runTheModel.m
@@ -0,0 +1,9 @@
+% comment to force rebuild of views
+model = 'mf14_mdl.mdl';
+simout = sim(model) %#ok<NOPTS> 
+
+% Comment we can add more text to with linux pipe to script lots of commits:
+% xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+% slcenter redeployment test - 8/30
+% 5/26/2023
+% TBE Demo - 6/27/2023
